79e050071cd6d81b5c451910bf45bc639031c883
Sebastián Claudio Nale <sebinale@gmail.com>
2024-02-19 11:32:34 -0300

    Adds another test that passes.

--- test/compressedRelay.ts
+++ test/compressedRelay.ts
@@ -7,6 +7,7 @@ import type { WormholeRelayerMock, RelayerGateway } from "../typechain-types"
 
 describe("RelayerGateway", () => {
 
+  const quorum = 13;
   let wormholeRelayerMock: WormholeRelayerMock;
   let relayerGateway: RelayerGateway;
   before(async () => {
@@ -27,7 +28,7 @@ describe("RelayerGateway", () => {
 
   describe("Relay GR message", () => {
     it("relay message should succeed", async () => {
-      const {message, timestamp, sequence} = buildMockMessage(13);
+      const {message, timestamp, sequence} = buildMockMessage(quorum);
 
       const tx = await relayerGateway.deliverGR(message);
       const receipt = await tx.wait();
@@ -44,22 +45,38 @@ describe("RelayerGateway", () => {
 
 
     it("malformed relay message should fail", async () => {
-      const {message} = buildMockMessage(16);
+      const {message} = buildMockMessage(quorum + 3);
 
       await expect(relayerGateway.deliverGR(message)).to.be.reverted;
     });
 
 
     it("relay message without sufficient signatures should fail", async () => {
-      const {message} = buildMockMessage(6);
+      const {message} = buildMockMessage(quorum - 7);
 
       await expect(relayerGateway.deliverGR(message)).to.be.reverted;
+    });
+
+    it("relay message with smaller payload should succeed", async () => {
+      const {message, timestamp, sequence} = buildMockMessage(quorum, 100);
+
+      const tx = await relayerGateway.deliverGR(message);
+      const receipt = await tx.wait();
+
+      assert.isNotNull(receipt);
+      assert.equal(receipt!.status, 1);
+      const events = receipt!.logs.map((log) => wormholeRelayerMock.interface.parseLog(log));
+      assert.lengthOf(events, 1);
+      const deliveryEvent = events[0];
+      assert.isNotNull(deliveryEvent);
+      assert.equal(deliveryEvent!.args.timestamp, timestamp);
+      assert.equal(deliveryEvent!.args.sequence, sequence);
     });
   });
 
 });
 
-function buildMockMessage(amountOfSignatures: number) {
+function buildMockMessage(amountOfSignatures: number, payloadSize = 200) {
   const mockSignature = new Array(66);
   mockSignature.fill(1, 0, mockSignature.length);
   const mockSignatures = [];
@@ -71,7 +88,12 @@ function buildMockMessage(amountOfSignatures: number) {
   const emitterChainId = 6;
   const sequence = 6000;
   const consistencyLevel = 1;
-  const payload = Uint8Array.from([2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0]);
+
+  const payloadData = [];
+  for (let i = 0; i < payloadSize; ++i) {
+    payloadData.push(i * 2 % 4);
+  }
+  const payload = Uint8Array.from(payloadData);
 
   const message = hre.ethers.solidityPacked(
     ["bytes", "uint32", "uint16", "uint64", "uint8", "uint16", "bytes"],
